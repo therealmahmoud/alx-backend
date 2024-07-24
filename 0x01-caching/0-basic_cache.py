2505489a6a5f1985b4b3a86cccc5c69a2d68625d
therealmahmoud <moha91875460@gmail.com>
2024-07-24 06:33:21 +0300

    Updated

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,6 +12,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Getting elments from cache."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
