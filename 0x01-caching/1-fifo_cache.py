3901f87b4db44c39f5971d286d58be3e65a558ed
therealmahmoud <moha91875460@gmail.com>
2024-07-24 11:42:56 +0300

    Updated

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" FIFOCache class. """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class."""
+    def put(self, key, item):
+        """ Putting elments in cache."""
+        if key is None or item is None:
+            return
+        print(len(self.cache_data))
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_item = list(self.cache_data.items())[0]
+            key_to_remove = first_item[0]
+            remove_val = self.cache_data.pop(key_to_remove)
+            print(f"DISCARD: {key_to_remove}")
+
+    def get(self, key):
+        """ Getting elments from cache."""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
