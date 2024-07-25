c6a4ca943ba2ceeb2558df13d11427e6bd72a05b
therealmahmoud <moha91875460@gmail.com>
2024-07-25 05:09:09 +0300

    Updated

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,9 +16,9 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                lru_key = self.cache_data.popitem()
+                print("DISCARD:", lru_key[0])
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
@@ -26,6 +26,6 @@ class LRUCache(BaseCaching):
 
     def get(self, key):
         """ Getting elments from cache."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
