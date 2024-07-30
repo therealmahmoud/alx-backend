094c209a44651e7b1d41ef77af6be2574626a4c6
therealmahmoud <moha91875460@gmail.com>
2024-07-31 00:48:33 +0300

    Updated

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 app.config['BABLE_DEFAULT_LOCALE'] = "en", "UTC"
 Babel = Babel(app)
 
+
 @app.route('/')
 def index() -> str:
     """ Home route"""
     return render_template('0-index.html')
 
+
 class Config:
     LANGUAGES = ["en", "fr"]
+
 
 if __name__ == "__main__":
     app.run(debug=True)
