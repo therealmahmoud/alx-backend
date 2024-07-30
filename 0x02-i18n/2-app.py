437634f7780e9c36a42e2bcc554617f117f1f156
therealmahmoud <moha91875460@gmail.com>
2024-07-31 01:01:34 +0300

    Updated

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Flask app."""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Flask babel config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match([Config.LANGUAGES])
+
+
+@app.route('/')
+def index() -> str:
+    """ Home route"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
