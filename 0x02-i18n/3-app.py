936f7c86a88cc5a4a477033dc44922c6a61c8e4d
therealmahmoud <moha91875460@gmail.com>
2024-07-31 02:09:35 +0300

    Updated

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app."""
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
