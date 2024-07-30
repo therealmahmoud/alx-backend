272d0a6018cd2d23bad776db8d106866df7bf853
therealmahmoud <moha91875460@gmail.com>
2024-07-31 02:28:50 +0300

    Updated

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Flask app."""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """ Flask babel config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Returns the locale of the web page."""
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
+    return request.accept_languages.best_match([Config.LANGUAGES])
+
+
+@app.route('/')
+def index() -> str:
+    """ Home route"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
